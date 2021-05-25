7281e38c3a34b7907fa15fcbb4baf7e0dd125914
leeroy Phili <leeroy@myshoperon.com>
2021-05-25 10:10:07 -0400

    used react query to fetch and cache data with user input

---
+++ src/components/FetchBooks/FetchBooks.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import { useQuery } from "react-query";
+
+import "./fetchBooks.scss"
+
+const BookSearch = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [books, setBooks] = useState([]);
+  const [sortType, setSortType] = useState("");
+  const [newdata, setNewData] = useState([]);
+
+  const { status, data, refetch } = useQuery(
+    [inputValue],
+    () =>
+      fetch(`http://openlibrary.org/search.json?title=${inputValue}`)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        }),
+    {
+      refetchAllOnWindowFocus: false,
+      enabled: false,
+    }
+  );
+
+  console.log(books);
+  console.log("data", data);
+
+ 
+
+
+  if (status === "loading") return <div>loading...</div>;
+  if (status === "error") return <div>There are no books matching your search please be more specific</div>;
+
+  const searchFunc = (e) => {
+    e.preventDefault();
+    // setNewData([])
+    setBooks([]);
+    setSortType("");
+    refetch();
+  };
+  return (
+    <div className="Search-books">
+      <h1 className="Search-books-header">Open Library Book Search</h1>
+      <div>
+        <SearchBar
+          value={inputValue}
+          onchange={(e) => setInputValue(e.target.value)}
+          submit={searchFunc}
+          className="Search-books-searchbar"
+        />
+       </div>
+       </div>
+  )
+};
+
+export default BookSearch;
